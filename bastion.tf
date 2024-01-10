91a2e847acd6c43f8b6e122906adf9750e4ccc17
Patrick Hardy <42849961+pahardy@users.noreply.github.com>
2024-01-10 18:57:38 -0500

    Update bastion.tf correct issue with SSH port reference in ingress rule

--- bastion.tf
+++ bastion.tf
@@ -15,7 +15,7 @@ module "sg-allow-ssh" {
   description = "Allow SSH access to the bastion host"
   vpc_id = module.vpc.vpc_id
   ingress_cidr_blocks = [ "0.0.0.0/0" ]
-  ingress_rules = [ "ssh-22-tcp" ]
+  ingress_rules = [ "ssh-tcp" ]
   egress_rules = [ "all-all"]
 }
 
